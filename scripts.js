a10bd64386cd88bbee1a27e4b189fe1906d59592
lionceptor <31250153+lionceptor@users.noreply.github.com>
2017-10-17 22:20:57 -0500

    added js file - buttons

---
+++ scripts.js
@@ -0,0 +1,15 @@
+//scripts
+//javascript folder
+//Lyon lee
+//changes color of a paragraph under header and reveals it too
+
+function textReveal()
+{
+	document.getElementById("change").style.fontSize = "50px";
+	document.getElementById("change").style.color = "white";
+}
+
+function textColor()
+{
+	document.getElementById("change").style.color = "red";
+}
